3b5612d70f0245e1cb2c82efef8e5d2022b91372
huawillian <huawillian@gmail.com>
2019-07-05 15:43:05 -0400

    Added time render for focus and break

--- public/js/detail.js
+++ public/js/detail.js
@@ -8,6 +8,39 @@ let stopEl;
 let backEl;
 
 let itemId = location.search.slice(1).split('&').find(param => param.startsWith('id')).split('=')[1];
+let originalFocusTime;
+let focusTimeLapse = 0;
+let originalBreakTime;
+let breakTimeLapse = 0;
+
+let intervalRef;
+
+const formatDigitToString = (num) => {
+  if(num < 10) {
+    return '0' + num.toString();
+  } else {
+    return num.toString();
+  }
+}
+
+const getHour = (milliseconds) => {
+  return formatDigitToString(Math.floor((milliseconds % 86400000) / 3600000));
+}
+
+const getMin = (milliseconds) => {
+  return formatDigitToString(Math.floor((milliseconds % 3600000) / 60000));
+}
+
+const getSec = (milliseconds) => {
+  return formatDigitToString(Math.floor((milliseconds % 60000) / 1000));
+}
+
+const renderTime = () => {
+  let focusTime = originalFocusTime - focusTimeLapse;
+  focusTimeEl.innerText = `${getHour(focusTime)}:${getMin(focusTime)}:${getSec(focusTime)}`;
+  let breakTime = originalBreakTime - breakTimeLapse;
+  breakTimeEl.innerText = `${getHour(breakTime)}:${getMin(breakTime)}:${getSec(breakTime)}`;
+}
 
 window.deleteItem = () => {
   console.log('deleting item!');
@@ -15,10 +48,24 @@ window.deleteItem = () => {
 
 window.startItem = () => {
   console.log('starting item!');
+  intervalRef = setInterval(() => {
+    if(focusTimeLapse < originalFocusTime) {
+      focusTimeLapse += 1000;
+    } else if(breakTimeLapse < originalBreakTime) {
+      breakTimeLapse += 1000;
+    } else {
+      clearInterval(intervalRef);
+    }
+    renderTime();
+  }, 1000);
 };
 
 window.stopItem = () => {
   console.log('stop item!');
+  clearInterval(intervalRef);
+  focusTimeLapse = 0;
+  breakTimeLapse = 0;
+  renderTime();
 };
 
 window.addEventListener("load", () => {
@@ -39,8 +86,11 @@ window.addEventListener("load", () => {
         console.log('Loading Item:', item);
         iconEl.innerText = item.icon;
         titleEl.innerText = item.title;
-        focusTimeEl.innerText = item.focusTime;
-        breakTimeEl.innerText = item.breakTime;
+
+        originalFocusTime = item.focusTime;
+        originalBreakTime = item.breakTime;
+
+        renderTime();
       } else {
         // TODO: Display error and link back to home
       }
